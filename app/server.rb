bda7590a5467b1b6845d333467c793a96a85a6fb
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-07 17:08:06 +0100

    User can sign out

--- app/server.rb
+++ app/server.rb
@@ -4,8 +4,9 @@ require 'rack-flash'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
-require_relative 'helpers/application'
+
 require_relative 'data_mapper_setup'
+require_relative 'helpers/application'
 
 enable :sessions
 set :session_secret, 'super secret'
@@ -64,5 +65,9 @@ post '/sessions' do
 		flash[:errors] = ["The email or password is incorrect"]
 		erb :"sessions/new"
 	end
+end
+
+delete '/sessions' do
+	"Good bye!"
 end
 
