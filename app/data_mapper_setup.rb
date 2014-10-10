d89f4801ecd2dca355a086f985147aff1db07912
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-10 10:53:15 +0100

    Updated datamapper setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,13 @@ require './app/models/user'
 require './app/models/tag'
 
 env = ENV["RACK_ENV"] || "development"
+# 'DATABASE_URL' = 'HEROKU_POSTGRESQL_CHARCOAL_URL' if ENV["RACK_ENV"] == 'production'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
+                             "postgres://localhost/bookmark_manager_#{env}")
 
 DataMapper.finalize
+
+
